c1afa45f82d5d722f324a8f9c928d3888c047ebd
Rozi Tunyan <SleepyR>
2019-05-03 20:04:44 +0400

    Update

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
-var cards;
-let index;
-let past = -1, last, count = 0;
+var cards, index;
+let past, last, count = 0;
 let start = true;
+let tID;
+let current = [];
+
 function reveal(id) {
-    if(start){
-        cards = ["Card1.png", "Card2.png", "Card3.png", "Card4.png", "Card5.png", "Card6.png", "Card7.png", "card.jpg","Card1.png", "Card2.png", "Card3.png", "Card4.png", "Card5.png", "Card6.png", "Card7.png", "card.jpg"];
+    if (start) {
+        cards = ["cards/Card1.png", "cards/Card2.png", "cards/Card3.png", "cards/Card4.png", "cards/Card5.png", "cards/Card6.png", "cards/Card7.png", "cards/card.jpg", "cards/Card1.png", "cards/Card2.png", "cards/Card3.png", "cards/Card4.png", "cards/Card5.png", "cards/Card6.png", "cards/Card7.png", "cards/card.jpg"];
         start = false;
+        count = 0;
     }
     if (!document.getElementById(id).children[0].style.backgroundImage) {
         index = parseInt(Math.random() * cards.length);
-        document.getElementById(id).children[0].style.backgroundImage = "url("+cards[index]+")";
+        document.getElementById(id).children[0].style.backgroundImage = "url(" + cards[index] + ")";
         document.getElementById(id).children[0].style.backgroundSize = "150px 210px";
         if (index !== -1) {
             cards.splice(index, 1);
         }
     }
+    if (document.getElementsByTagName("td")[id - 1].classList.contains("flip")) {
+        return;
+    }
     document.getElementsByTagName("td")[id - 1].classList.add("flip");
-    if (past === -1) {
-        last = id;
-        past = last;
-    } else {
-        past = last;
-        last = id;
-        check(past, last);
-        past = -1;
+    current.push(id);
+    if (current.length === 2) {
+        check(current[0],current[1]);
+        current = [];
     }
 }
 
 function check(x, y) {
-     if (document.getElementById(x).children[0].style.backgroundImage === document.getElementById(y).children[0].style.backgroundImage) {
-        console.log(true);
+    if (document.getElementById(x).children[0].style.backgroundImage === document.getElementById(y).children[0].style.backgroundImage) {
         count++;
-        console.log(cards);
         if (count === 8) {
-            alert("WIN");
+            setTimeout(function(){alert("WIN")
+                for (var i = 1; i <= 16; i++) {
+                    document.getElementsByTagName("td")[i - 1].classList.remove("flip");
+                    document.getElementById(i).children[0].style.backgroundImage = "";
+                }
+            }, 1000);
             start = true;
-            for (var i = 1; i <= 16; i++) {
-                document.getElementsByTagName("td")[i-1].classList.remove("flip");
-                document.getElementById(i).children[0].style.backgroundImage = "";
-            }
+
         }
     } else {
-        setTimeout(document.getElementsByTagName("td")[past-1].classList.remove("flip"),1000);
-        setTimeout(document.getElementsByTagName("td")[last-1].classList.remove("flip"),2000);
-        console.log(false);
+        document.getElementsByTagName("td")[x - 1].classList.remove("flip");
+        document.getElementsByTagName("td")[y - 1].classList.replace("flip","flipBack");
+
     }
 }
 
