1763c24a229689eb820caf66b46585fb86c698ca
Rozi Tunyan <SleepyR>
2019-05-06 18:57:28 +0400

    changes to the logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 var cards, index;
 let past, last, count = 0;
 let start = true;
@@ -6,57 +9,116 @@ let current = [];
 
 function reveal(id) {
     if (start) {
-        cards = ["cards/Card1.png", "cards/Card2.png", "cards/Card3.png", "cards/Card4.png", "cards/Card5.png", "cards/Card6.png", "cards/Card7.png", "cards/card.jpg", "cards/Card1.png", "cards/Card2.png", "cards/Card3.png", "cards/Card4.png", "cards/Card5.png", "cards/Card6.png", "cards/Card7.png", "cards/card.jpg"];
+        cards = cards = [
+            {
+                id: "1",
+                value: "cards/Card1.png"
+            },
+            {
+                id: "2",
+                value: "cards/Card2.png"
+            },
+            {
+                id: "3",
+                value: "cards/Card3.png"
+            },
+            {
+                id: "4",
+                value: "cards/Card4.png"
+            },
+            {
+                id: "5",
+                value: "cards/Card5.png"
+            },
+            {
+                id: "6",
+                value: "cards/Card6.png"
+            },
+            {
+                id: "7",
+                value: "cards/Card7.png"
+            },
+            {
+                id: "8",
+                value: "cards/card.jpg"
+            },
+            {
+                id: "9",
+                value: "cards/Card1.png"
+            },
+            {
+                id: "10",
+                value: "cards/Card2.png"
+            },
+            {
+                id: "11",
+                value: "cards/Card3.png"
+            },
+            {
+                id: "12",
+                value: "cards/Card4.png"
+            },
+            {
+                id: "13",
+                value: "cards/Card5.png"
+            },            {
+                id: "14,
+                value: "cards/Card6.png"
+            },
+            {
+                id: "15",
+                value: "cards/Card7.png"
+            },
+            {
+                id: "16",
+                value: "cards/card.jpg"
+            }];
         start = false;
         count = 0;
     }
-    element = document.getElementById(id).children[0].style;
-    if (!element.backgroundImage) {
+    element = document.getElementById(id).children[0];
+    if (!element.style.backgroundImage) {
         index = parseInt(Math.random() * cards.length);
-        element.backgroundImage = "url(" + cards[index] + ")";
-        element.backgroundSize = "150px 210px";
+        element.style.backgroundImage = "url(" + cards[index].value + ")";
+        element.style.backgroundSize = "150px 210px";
         if (index !== -1) {
             cards.splice(index, 1);
         }
+        current.push(cards[index]);
     }
-    cell = document.getElementsByTagName("td")[id - 1];
-    if (cell.classList.contains("flipBack")) {
-        cell.classList.remove("flipBack");
-        void cell.offsetWidth;
-    }
-    if (!cell.classList.contains("flip")) {
-        cell.classList.add('flip');
-        current.push(id);
-    }
+    // if (cell.classList.contains("flipBack")) {
+    //     cell.classList.remove("flipBack");
+    //     void cell.offsetWidth;
+    // }
+    // if (!cell.classList.contains("flip")) {
+    //     cell.classList.add('flip');
+    //     current.push(id);
+    // }
     if (current.length === 2) {
-        console.log(current[0],current[1]);
         if(!(current[0] === current[1])){
             check(current[0],current[1]);
             current = [];
         }
-        else 
+        else
             current = [];
     }
-    
+
 }
 
 function check(x, y) {
-    if (document.getElementById(x).children[0].style.backgroundImage === document.getElementById(y).children[0].style.backgroundImage) {
+    if (x.value === y.value) {
         count++;
         if (count === 8) {
-            setTimeout(function(){
-                alert("WIN");
-                for (var i = 1; i <= 16; i++) {
-                    document.getElementsByTagName("td")[i - 1].classList.add('flipBack');
-                    document.getElementById(i).children[0].style.backgroundImage = "";
-                }
-            }, 1000);
+            alert("WIN");
+            for (var i = 1; i <= 16; i++) {
+                document.getElementsByTagName("td")[i - 1].classList.add('flipBack');
+                document.getElementById(i).children[0].style.backgroundImage = "";
+            }
             start = true;
         }
     }
-     else {
+    else {
         document.getElementsByTagName("td")[y - 1].classList.replace("flip","flipBack");
         document.getElementsByTagName("td")[x - 1].classList.replace("flip","flipBack");
     }
 }
-
